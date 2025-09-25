19b12311ac13d9331e2641b8cbacc973b4205ffc
YuLap <yukinenelapin42@gmail.com>
2025-09-26 00:31:46 +0300

    lab 1, 2, 3

---
+++ shared/js/head.js
@@ -0,0 +1,6 @@
+fetch('/shared/html/head.html')
+  .then(r => r.text())
+  .then(html => {
+    document.head.insertAdjacentHTML('afterbegin', html);
+  })
+  .catch(err => console.error('Error loading head:', err));
